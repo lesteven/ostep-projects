ab6e9d7698140307a191c8427a3cc26fcc8451e7
lesteven <stevenle2011@gmail.com>
2020-04-26 15:59:20 -0700

    finish wcat

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+        char *line = NULL;
+        size_t size = sizeof(&line);
+        while(getline(&line, &size , fp) != -1) {
+            printf("%s",line);
+        }
+        fclose(fp);
     }
     return 0;
 }
