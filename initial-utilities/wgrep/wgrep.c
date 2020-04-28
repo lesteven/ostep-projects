164d93f43f3c577a779c2c7e0f25281cf79aaf2a
lesteven <stevenle2011@gmail.com>
2020-04-27 23:12:38 -0700

    finish wgrep

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -13,6 +13,18 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    // if search term given, but no file selected
+    if (argc == 2) {
+        char *line = NULL;
+        size_t size = sizeof(&line);
+        while(getline(&line, &size, stdin) != -1) {
+            char *found = strstr(line, searchTerm);
+            if (found != NULL) {
+                printf("%s", line);
+            }
+        }
+    }
+
     // find search terms if files are provided
     for (int i = 2; i < argc; i++) {
         FILE *fp = fopen(argv[i], "r");
