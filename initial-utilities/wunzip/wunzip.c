39083c6b8b99ca6b2a7653324ce9fcad8d35b745
lesteven <stevenle2011@gmail.com>
2020-05-02 18:19:36 -0700

    add wunzip

---
+++ initial-utilities/wunzip/wunzip.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        while (1) {
+            int count = 0;
+            fread(&count, 4, 1, fp);
+
+            if (count == 0) {
+                break;
+            }
+            int letter = 0;
+            fread(&letter, 1, 1, fp);
+            for (int i = 0; i < count; i++) {
+                printf("%c", letter);
+            }
+        }
+    }
+    return 0;
+}
