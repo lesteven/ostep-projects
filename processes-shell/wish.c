5139d391cf939cbf18ed7ff9e449845fac3b6c97
lesteven <stevenle2011@gmail.com>
2020-05-09 19:21:51 -0700

    wip: wish.c add prompt

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void executeLine(FILE *stream) {
+    char *line = NULL;
+    size_t size = 0;
+    getline(&line, &size, stream);
+    printf("wish> command entered: %s", line);
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         // run interactive mode
+        while (1) {
+            printf("wish> ");
+            executeLine(stdin);
+        }
 
     } else if (argc > 2) {
-        fprintf(stderr,"wish shell only accepts one argument\n");
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
     } else {
         // batch mode
         FILE *read_file = fopen(argv[1], "r");
         if (read_file == NULL) {
-            fprintf(stderr, "wish: cannot open file '%s'\n", argv[1]);
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
             exit(1);
         }
 
