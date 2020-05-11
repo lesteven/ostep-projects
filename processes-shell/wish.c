035939fdf3cf8ac2683bbe2eeee67e6016fdce49
lesteven <stevenle2011@gmail.com>
2020-05-10 21:35:00 -0700

    wip: add ls to wish.c

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -3,11 +3,59 @@
 #include <string.h>
 #include <unistd.h>
 
+char pathOne[] = "/bin";
+char pathTwo[] = "/usr/bin";
+char fslash[] = "/";
+
+void getCommand(char *command, char *path, char *split) {
+    // add +1 -> copies '\n', prevents junk data from showing
+    strncpy(command, path, strlen(path)+1);
+    strncat(command, fslash, strlen(fslash));
+    strncat(command, split, strlen(split));
+    /*
+    for (int i = 0; i < strlen(command); i++) {
+        printf("%c %d %d\n", command[i], command[i], i);
+    }
+    printf("==============");
+    */
+}
+
+void trimStr(char str[]) {
+    for (int i = strlen(str)-1; i >=0; i--) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+        }
+    }
+}
+
 void executeLine(FILE *stream) {
     char *line = NULL;
     size_t size = 0;
     getline(&line, &size, stream);
-    printf("wish> command entered: %s", line);
+    char *split = NULL;
+    while (line != NULL) {
+        split = strsep(&line, "&");
+        trimStr(split);
+        // +1 for '\0' character
+        char command[strlen(pathOne)+strlen(fslash)+strlen(split)+1];
+        getCommand(command, pathOne, split);
+
+        /*
+        int size = strlen(pathOne)+strlen(fslash)+strlen(split);
+        printf("%s %d\n", command, size);
+        int test = access(command, X_OK);
+
+        char *foo = "/bin/ls";
+        int test2 = access(foo, X_OK);
+        int cmp = strcmp(foo, command);
+        //printf("run: %s %d %d %d %d\n", command, test, test2, cmp, size);
+
+        //printf("%s %ld\n", command, strlen(command));
+        //printf("%s %ld\n", foo, strlen(foo));
+        */
+        char *files[] = { split, NULL };
+        execv(command, files);
+    }
 }
 
 int main(int argc, char *argv[]) {
